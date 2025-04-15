099e8b04857808002fcb6933c00a3e89c627e819
Vũ Lưu <vuluu@Vus-Mac-mini.local>
2025-04-16 00:02:41 +0700

    update

--- variable/target/debug/deps/variable-8a2d6976d121b8a8.d
+++
@@ -1,5 +0,0 @@
-/Users/vuluu/Documents/workspace/rust_test/variable/target/debug/deps/variable-8a2d6976d121b8a8: src/main.rs
-
-/Users/vuluu/Documents/workspace/rust_test/variable/target/debug/deps/variable-8a2d6976d121b8a8.d: src/main.rs
-
-src/main.rs:
--- variable/target/debug/variable.d
+++
@@ -1,1 +0,0 @@
-/Users/vuluu/Documents/workspace/rust_test/variable/target/debug/variable: /Users/vuluu/Documents/workspace/rust_test/variable/src/main.rs
